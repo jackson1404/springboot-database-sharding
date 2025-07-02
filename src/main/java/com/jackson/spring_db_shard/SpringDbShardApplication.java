8836cc848bbcb14cd099daf50a46941193943578
Win Min Thant <jackson14042004@gmail.com>
2025-07-02 11:10:47 +0630

    first commit frame

---
+++ src/main/java/com/jackson/spring_db_shard/SpringDbShardApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.spring_db_shard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbShardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbShardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackson/spring_db_shard/SpringDbShardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.spring_db_shard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbShardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
